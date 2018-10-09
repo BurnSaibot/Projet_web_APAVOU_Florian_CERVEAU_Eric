057e8834cb15c1e6ba3cfd095536506503c30a42
Eokaun <florian.apavou@u-psud.fr>
2018-10-09 17:31:39 +0200

    MAJ de menu.php, menu.js et style.css

--- php/menu.php
+++ php/menu.php
@@ -7,45 +7,117 @@ $pseudo = $_REQUEST["pseudo"];
 <head>
 	<title>Accueil</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+	<script src="../js/menu.js" type="text/javascript"></script>
 </head>
 <body>
-	<p><?php echo $pseudo ?></p> 
-	<a href="welcome.php" class="btn btn-danger" role="button">Déconnexion</a>
-	<h1>Home</h1>
-	<a href="move.php" class="btn btn-info" role="button">Enregistrer un nouveau mouvement</a>
-	<hr>
-	<h2>Recherche</h2>
-	<form>
-		Pseudo :
-		<input type="text" name="">
-		Mots-clé :
-		<input type="text" name="">
-		Date :
-		<input type="date" name="">
-		Durée :
-		<input type="number" name="duration">
-	</form>
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
-			<div class="col-sm-6 col-md-4 col-lg-3"><img src="chart.png" class="img-thumbnail"></div>
+
+	<div class = "navbar navbar-default navbar-static banniere">
+
+		<div class ="user">
+			<span><?php echo $pseudo ?></span>
+			<a href="welcome.php" class="btn btn-danger" role="button">Déconnexion</a>
+		</div> 
+
+		<h1>Home</h1>
+
+		<div class = "new-move-btn">
+			<a href="../html/index.html" class="btn btn-info" role="button">Enregistrer un nouveau mouvement</a>
 		</div>
+
 	</div>
 
+	<div class = "search">
+
+		<h2>Recherche</h2>
+
+		<form>
+			Pseudo :
+			<input type="text" name="">
+			Mots-clé :
+			<input type="text" name="">
+			Date :
+			<input type="date" name="">
+			Durée :
+			<input type="number" name="duration">
+		</form>
+
+	</div>
+
+	<div class="container-fluid">
+
+		<div class="row">
+			<div class="col-sm-5 col-md-4 col-lg-2 movebox">
+				<h2> Double Pas #1 </h2>
+				<h3>Michel Jordan</h3>
+				<p>double-pas</p>
+				<p>9/10/18</p>
+			</div>
+
+			<div class="col-sm-5 col-md-4 col-lg-2 movebox">
+				<h2> Double Pas #1 </h2>
+				<h3>Michel Jordan</h3>
+				<p>double-pas</p>
+				<p>9/10/18</p>
+			</div>
+
+			<div class="col-sm-5 col-md-4 col-lg-2 movebox">
+				<h2> Double Pas #1 </h2>
+				<h3>Michel Jordan</h3>
+				<p>double-pas</p>
+				<p>9/10/18</p>
+			</div>
+
+			<div class="col-sm-5 col-md-4 col-lg-2 movebox">
+				<h2> Double Pas #1 </h2>
+				<h3>Michel Jordan</h3>
+				<p>double-pas</p>
+				<p>9/10/18</p>
+			</div>
+
+			<div class="col-sm-5 col-md-4 col-lg-2 movebox">
+				<h2> Double Pas #1 </h2>
+				<h3>Michel Jordan</h3>
+				<p>double-pas</p>
+				<p>9/10/18</p>
+			</div>
+
+			<div class="col-sm-5 col-md-4 col-lg-2 movebox">
+				<h2> Double Pas #1 </h2>
+				<h3>Michel Jordan</h3>
+				<p>double-pas</p>
+				<p>9/10/18</p>
+			</div>
+
+			<div class="col-sm-5 col-md-4 col-lg-2 movebox">
+				<h2> Double Pas #1 </h2>
+				<h3>Michel Jordan</h3>
+				<p>double-pas</p>
+				<p>9/10/18</p>
+			</div>
+
+			<div class="col-sm-5 col-md-4 col-lg-2 movebox">
+				<h2> Double Pas #1 </h2>
+				<h3>Michel Jordan</h3>
+				<p>double-pas</p>
+				<p>9/10/18</p>
+			</div>
+
+			<div class="col-sm-5 col-md-4 col-lg-2 movebox">
+				<h2> Double Pas #1 </h2>
+				<h3>Michel Jordan</h3>
+				<p>double-pas</p>
+				<p>9/10/18</p>
+			</div>
+		<div>
+	</div>
+
+	<div>
+		<script>
+			readJSON("../js/sample.json");
+ 		</script>
+	</div>
+
+	
 	</html>
