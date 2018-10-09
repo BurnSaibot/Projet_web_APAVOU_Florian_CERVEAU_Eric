91190ff5b8bdcc5c7a2b610f8222de3f70a55fd2
Eokaun <florian.apavou@u-psud.fr>
2018-10-09 14:15:21 +0200

    Ajout de nouveaux dossiers et fichiers
    Coucou on test un truc

---
+++ php/welcome.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Connexion</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+	<h1>Connexion au Projet Basket</h1>
+	<hr>
+	<form method="post" action="menu.php">
+		Entrez votre pseudo :
+		<br>
+		<input type="text" name="pseudo">
+		<br>
+		<button type="submit" class="btn btn-info">Connexion</button>
+	</form>
+</body>
+</html>
