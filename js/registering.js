30dc231fe9679f411e0148d68a63f9471ffeff4f
Cerveau Eric <eric.cerveau@hotmail.fr>
2018-10-24 16:21:12 +0200

    updt format registering -> cookies & datas

--- js/registering.js
+++ js/registering.js
@@ -35,7 +35,7 @@ function motion(event){
 }
 
 function processData() {
-	var dataToPost = {datas: [],cookies: []};
+	var dataToPost = {datas: {},cookies: {}};
 	dataToPost.datas = register.records;
 	dataToPost.cookies = getCookiesData()
 	//$('.container').append("<div> " + JSON.stringify(dataToPost) + "</div>");
@@ -66,25 +66,24 @@ function testCookies() {
 }
 
 function getCookiesData() {
-	var finalData = {records :[]};
+	var tab;
 	var decodedCookie = decodeURIComponent(document.cookie);
 	var datas = decodedCookie.split(';');
 	for (var i = 0; i < datas.length ; i++) {
 		var item = datas[i].split('=');
         if (item[0].includes("user"))
-        	finalData.records.push({user: item[1]});
+        	tab.push(item[1]);
         else if (item[0].includes("name"))
-        	finalData.records.push({name:  item[1]});
+        	tab.push(item[1]);
         else if (item[0].includes("date"))
-        	finalData.records.push({date : item[1]});
+        	tab.push(item[1]);
         else if (item[0].includes("keyWords"))
-        	finalData.records.push({keyWords: item[1]});
+        	tab.push(item[1]);
         else if (item[0].includes("timeDuration")) {
-        	finalData.records.push({timeDuration: item[1]});
+        	tab.push(item[1]);
 		}
-		finalData.records.push({id : -1});
 	}
-	return finalData.records;
+	return {user : tab[0], name : tab[1], date : tab[2], keyWords : tab[3], duration : tab[4]};
 }
 
 function displayCookies() {
