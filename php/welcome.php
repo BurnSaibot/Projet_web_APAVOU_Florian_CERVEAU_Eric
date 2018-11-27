d6cd25182ce32c6ab0b1dbf9dff7cc17116c6eb0
Eokaun <florian.apavou@u-psud.fr>
2018-11-27 16:12:28 +0100

    Modification mineures CSS

--- php/welcome.php
+++ php/welcome.php
@@ -3,16 +3,21 @@
 <head>
 	<title>Connexion</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
 <body>
-	<h1>Connexion au Projet Basket</h1>
-	<hr>
-	<form method="post" action="menu.php">
-		Entrez votre pseudo :
-		<br>
-		<input type="text" name="pseudo">
-		<br>
-		<button type="submit" class="btn btn-info">Connexion</button>
-	</form>
+	<div class="navbar navbar-default banniere">
+		<h1>Connexion au Projet Basket</h1>
+	</div>
+
+	<div class="connexionDiv">
+		<form method="post" action="menu.php">
+			Entrez votre pseudo :
+			<br>
+			<input type="text" name="pseudo" required>
+			<br>
+			<button type="submit" class="btn btn-info">Connexion</button>
+		</form>
+	</div>
 </body>
 </html>
