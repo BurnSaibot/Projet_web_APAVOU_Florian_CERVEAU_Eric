2c8d3d3b9b2f8fe77c960cd0d186874ad8349674
Eric Cerveau <eric.cerveau@a-018818.ups.u-psud.fr>
2018-12-04 17:07:13 +0100

    Recording time to allow drawing on canvas

--- js/registering.js
+++ js/registering.js
@@ -18,7 +18,7 @@ function switchRecord() {
 		endDate = new Date();
 		recording = false;
 		clearTimeout(buttonTimeout);
-		var time  = Math.abs(endDate.getTime() - startDate.getTime());;
+		var time  = Math.abs(endDate.getTime() - startDate.getTime());
 		Cookies.set("timeDuration", time, { expires: 7 });
 		processData();
 					
@@ -32,9 +32,12 @@ function switchRecord() {
 
 function motion(event){
 	if (recording) {
+		let currTime = new Date();
+		timer = Math.abs(currTime.getTime() - startDate.getTime())
 		register.records.push({
 			acceleration: {x: event.acceleration.x, y: event.acceleration.y , z : event.acceleration.z},
-			rotation: {alpha: event.rotationRate.alpha, beta: event.rotationRate.beta, gamma: event.rotationRate.gamma}	
+			rotation: {alpha: event.rotationRate.alpha, beta: event.rotationRate.beta, gamma: event.rotationRate.gamma},
+			time: timer
 		})
 	}
 }
@@ -49,20 +52,6 @@ function processData() {
 	$.ajax({
 		type: 'POST',
 		url: "https://tp-ssh1.dep-informatique.u-psud.fr/~eric.cerveau/Projet_web_APAVOU_Florian_CERVEAU_Eric/php/savingRecords.php",
-		data : "data=" +  JSON.stringify(dataToPost),
-		success: function(data) {
-			alert("Vos données ont bien été envoyées au serveur");
-			$('.container').append(data);
-		},
-		error: function(xhr,textStatus,err)
-        {
-            alert("readyState: " + xhr.readyState +"\n"+ "responseText: "+ xhr.responseText +"\n" + "status: " + xhr.status +"\n" + "text status: " + textStatus + "\n" + "error: " + err);
-        }
-	})
-
-	$.ajax({
-		type: 'POST',
-		url: "https://tp-ssh1.dep-informatique.u-psud.fr/~fapavou/Projet_web_APAVOU_Florian_CERVEAU_Eric/php/savingRecords.php",
 		data : "data=" +  JSON.stringify(dataToPost),
 		success: function(data) {
 			alert("Vos données ont bien été envoyées au serveur");
