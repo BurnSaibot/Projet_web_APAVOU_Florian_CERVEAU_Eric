f9c875d5496569975991d7e2a6eebf621bf5380c
Eric Cerveau <ecervea@a-018818.ups.u-psud.fr>
2018-12-11 16:08:03 +0100

    PAs de changement dans registering pourtant

--- js/canvaProcessing.js
+++ js/canvaProcessing.js
@@ -19,8 +19,34 @@ function display1Canva() {
 }
 
 function displayCanva(data) {
-	$.each( data , function(key,val){
-		console.log("Acceleration : \n\t-x : " + val.acceleration.x + "\n\t-y : " + val.acceleration.y + "\n\t-z : " + val.acceleration.z)
-	})
 	$.(".canvasContainer").append('<canvas id="myCanvas" width="200" height="100"></canvas>');
+	let canvas = document.getElementById("myCanvas");
+	let ctx = canvas.getContext("2d");
+	let lastX = 0;
+	let lastY = 0;
+	let lastZ = 0;
+	let currentX = 0;
+	let currentY = 0;
+	let currentZ = 0;
+	ctx.moveTo(0,0);
+	$.each( data , function(key,val){
+		//console.log("Acceleration : \n\t-x : " + val.acceleration.x + "\n\t-y : " + val.acceleration.y + "\n\t-z : " + val.acceleration.z)
+		if (currentX == 0 && currentY == 0 && currentZ == 0){
+			currentX = val.acceleration.x;
+			currentY = val.acceleration.y;
+			currentZ = val.acceleration.z;
+		} else {
+			lastX = currentX;
+			lastY = currentY;
+			lastZ = currentZ;
+			currentX = val.acceleration.x;
+			currentY = val.acceleration.y;
+			currentZ = val.acceleration.z;
+		}
+		if (!(lastX == 0 & lastY == 0 & lastZ ==0)) {
+			context.strokeStyle = '#ff0000';
+			ctx.lineTo(val.time,val.acceleration.x);
+			ctx.stroke();
+		}
+	})
 }
