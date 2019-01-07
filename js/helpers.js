e9721e439d1b35596f315a3f3bef0693864ab9cc
Unknown <eric.cerveau@hotmail.fr>
2019-01-07 21:44:47 +0100

    Nouvelle version du formulaire 3

--- js/helpers.js
+++ js/helpers.js
@@ -68,8 +68,8 @@ function fillName() {
 
 function testContent() {
 	if ($(".name").val.length > 0 && $(".title").val.length > 0 && $(".k-word1").val.length > 0 ) {
-		$(':input[type=submit]').attr("disabled","disabled");
+		$(':input[type=submit]').prop("disabled","false");
 	} else {
-		$(':input[type=submit]').attr("disabled",false);
+		$(':input[type=submit]').prop("disabled","true");
 	}
 }
